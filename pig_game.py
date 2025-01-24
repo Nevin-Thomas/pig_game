55c3f23e9738130e34c97d993eb6b193c467e6c2
Nevin-Thomas <106274286+Nevin-Thomas@users.noreply.github.com>
2025-01-24 13:17:31 +0200

    Update pig_game

--- pig_game.py
+++ pig_game.py
@@ -22,6 +22,31 @@ max_score = 50
 player_scores = [0 for _ in range(players)] #list of all the scores for all players. Range will loop the number of players we have
 
 while max(player_scores) < max_score:
-    
+
+    for players_ind in range(players):
+        print("\nPlayer ", players_ind + 1, "turn has started\n")
+        current_score = 0
+
+        while True:
+            should_roll = input("Would you like to roll (y)? ")
+            if should_roll.lower() != "y":
+                break
+            
+            value = roll()
+            if value == 1 :
+                print("You rolled a 1! Turn done!")
+                current_score = 0
+                break
+            else:
+                current_score += value
+                print("You rolled a ", value)
+
+            print("Your current score is", current_score)
+
+        player_scores[players_ind] == current_score
+        print("Your total score is", player_scores[players_ind])
+
+
+
 
 
